6da1319c7daa88bf2b82d2f16bd655ae594bf514
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-10 09:57:33 -0500

    added java lecture, and completed java exercises

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int myFavoriteNumber;
+        myFavoriteNumber = 4;
+
+        String myString;
+        myString = "Fallout New Vegas";
+
+//        System.out.println(myString);
+
+        float myNumber;
+        myNumber = 3.14F;
+//        System.out.println(myNumber);
+
+        // post increment - adds one to x last, making it 5 still, then outputs 6 afterwards
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        // pre increment - adds one to x first, making it 6, then outputs 6
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x+=5;
+
+//        int x = 3;
+//        int y = 4;
+//        y*=x;
+
+        int x = 10;
+        int y = 2;
+        x/=y;
+        y-=x;
+
+        System.out.println(y);
+
+    }
+
+
+}
