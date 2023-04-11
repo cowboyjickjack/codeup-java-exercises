1789b4aae26f41b4b464f2a20d9f6e10c854faff
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-11 09:30:11 -0500

    updated control flow exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -29,46 +29,46 @@ public class ControlFlowExercises {
 //        do {
 //            System.out.println("i is " + i);
 //            i*=i;
-//        } while (i < 1000000);
+//        } while (i < 1_000_000);  // can separate with underscores to make it easier to read
 
         // FOR LOOP
 //        for (int i = 100; i >= -10; i -= 5) {
 //            System.out.println("i is " + i);
 //        }
 
-//        for (long i = 2; i < 100000; i *= i) {
+//        for (long i = 2; i < 1_000_000; i *= i) {
 //            System.out.println("i is " + i);
 //        }
 
-        // FIZZBUZZ
-//        for (int i = 0; i <= 100; i++) {
-//            if ((i % 3 == 0) && (i % 5 == 0)) {
-//                System.out.println("FIZZBUZZ");
-//            } else if (i % 3 == 0) {
-//                System.out.println("FIZZ");
-//            } else if (i % 5 == 0) {
-//                System.out.println("BUZZ");
-//            } else {
-//                System.out.println(i);
-//            }
-//        }
-
-        // TABLE OF POWERS
-        System.out.println("Please enter an integer:");
-        int userInteger = scanner.nextInt();
-
-        for (int i = 1; i <= userInteger; i++) {
-            int numSquared = (int)Math.pow(i, 2);
-            int numCubed = (int)Math.pow(i, 3);
-            String message = "Number: " + userInteger + " Squared: " + numSquared + " Cubed: " + numCubed;
-
-            System.out.println(message);
+        // FIZZBUZZ (USED A LOT IN TECHNICAL INTERVIEWS... BE VOCAL, AND VERBALIZE YOUR THOUGHT PROCESS!!!)
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
 
-        System.out.println("Would you like to continue? [Y/N]");
-        String userConfirmation = scanner.nextLine();
-        boolean confirmation = userConfirmation.equalsIgnoreCase("Y");
-        String userAnswer = scanner.nextLine();
-        System.out.println(userAnswer);
+        // TABLE OF POWERS
+//        System.out.println("Please enter an integer:");
+//        int userInteger = scanner.nextInt();
+//
+//        for (int i = 1; i <= userInteger; i++) {
+//            int numSquared = (int)Math.pow(i, 2);
+//            int numCubed = (int)Math.pow(i, 3);
+//            String message = "Number: " + userInteger + " Squared: " + numSquared + " Cubed: " + numCubed;
+//
+//            System.out.println(message);
+//        }
+//
+//        System.out.println("Would you like to continue? [Y/N]");
+//        String userConfirmation = scanner.nextLine();
+//        boolean confirmation = userConfirmation.equalsIgnoreCase("Y");
+//        String userAnswer = scanner.nextLine();
+//        System.out.println(userAnswer);
     }
 }
