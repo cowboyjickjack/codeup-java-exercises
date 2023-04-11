d89fb123dee7bd844f2a1a9eb5286ac169d3a87b
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-11 10:49:59 -0500

    completed bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ask Bob a question! :)");
+        String userInput = scanner.nextLine();
+
+        if (userInput.trim().toLowerCase().endsWith("?")) {
+            System.out.println("Sure.");
+        } else if (userInput.trim().toLowerCase().endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userInput.equals("") || (userInput.equals(" "))) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever.");
+        }
+    }
+}
