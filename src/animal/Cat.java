c0055d2b16ac397c80b7bac88413616d50259662
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-12 13:44:01 -0500

    completed mini exercise

--- src/animal/Cat.java
+++ src/animal/Cat.java
@@ -2,8 +2,22 @@ package animal;
 
 public class Cat {
 
-    public String name;
+    private String name;
     public String sound;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSound(){
+        return sound;
+    }
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
 
     public void makeNoise() {
         System.out.println(name + " goes " + sound + "!");
--- src/animal/test.java
+++ src/animal/test.java
@@ -3,8 +3,8 @@ package animal;
 public class test {
     public static void main(String[] args) {
         Cat cat = new Cat();
-        cat.name = "Kirk";
-        cat.sound = "meow";
+        cat.setName("Kirk");
+        cat.setSound("meow");
         cat.makeNoise();
     }
 }
