3302e22b2beee4b6165fe54d5bf74754180b04dc
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-12 12:18:46 -0500

    completed mini exercise

---
+++ src/animal/Cat.java
@@ -0,0 +1,11 @@
+package animal;
+
+public class Cat {
+
+    public String name;
+    public String sound;
+
+    public void makeNoise() {
+        System.out.println(name + " goes " + sound + "!");
+    }
+}
---
+++ src/animal/test.java
@@ -0,0 +1,10 @@
+package animal;
+
+public class test {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.name = "Kirk";
+        cat.sound = "meow";
+        cat.makeNoise();
+    }
+}
