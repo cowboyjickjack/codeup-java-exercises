5c7a68a6b54f1fd38c614f37556aac107775dd68
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-13 09:54:49 -0500

    continuing object oriented programming exercises

---
+++ src/shapes/Circle.java
@@ -0,0 +1,15 @@
+package shapes;
+
+public class Circle {
+
+    private int Radius;
+
+    public Circle(double radius){
+    }
+    public static double getArea(double radius){
+        return radius * radius * Math.PI;
+    }
+    public static double getCircumference(int radius){
+        return 2 * Math.PI * radius;
+    }
+}
---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,10 @@
+package shapes;
+
+public class CircleApp {
+    public static void main(String[] args) {
+
+
+        System.out.println(Circle.getArea(4));
+        System.out.println(Circle.getCircumference(4));
+    }
+}
