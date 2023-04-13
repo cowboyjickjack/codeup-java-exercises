d36e2e09fcdd39069443afb978397af7ed2984cf
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-13 15:18:19 -0500

    finished objects oriented exercises

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -5,7 +5,8 @@ import util.Input;
 public class CircleApp {
     public static void main(String[] args) {
 
-        System.out.println(Circle.getArea(4));
-        System.out.println(Circle.getCircumference(4));
+//        System.out.println(Circle.getArea(4));
+//        System.out.println(Circle.getCircumference(4));
+
     }
 }
