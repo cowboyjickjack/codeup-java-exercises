68ae881a8fd766fa1006bad01544e2b905517fc9
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-14 09:08:51 -0500

    continuing java arrays lecture

--- src/ArraysLecture.java
+++ src/ArraysLecture.java
@@ -3,12 +3,16 @@ public class ArraysLecture {
 
         int sizeOfArray = 5; // you can put a variable into an array
 
-        int[] numbers = new int[sizeOfArray]; // just declared an array of integers
+        // Array Variable Declaration
+        int[] numbers = new int[sizeOfArray];
         numbers[0] = 42;
         numbers[1] = 567;
         numbers[2] = 457;
         numbers[3] = -560;
         numbers[4] = 1267;
+
+        // Array Initializer Syntax
+        int[] moreNumbers = {45, 56, 67, 78};
 
         for (int number : numbers){
             System.out.println(numbers);
