020a77d7b62788d580d0442b5848503cc95ca0e6
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-19 12:01:51 -0500

    updated hashmaps lecture

--- src/collections/CollectionsLecture.java
+++ src/collections/CollectionsLecture.java
@@ -4,6 +4,8 @@ import rpg.Monster;
 
 import java.util.*;
 
+import static java.util.Map.entry;
+
 public class CollectionsLecture {
 
     public static void main(String[] args) {
@@ -11,6 +13,9 @@ public class CollectionsLecture {
         Monster orc = new Monster("Orc", 13, 15, 9);
         Monster ogre = new Monster("Ogre", 11, 59, 13);
         Monster blueDragon = new Monster("Blue Dragon", 19, 225, 23);
+        Monster frostGiant = new Monster("Frost Giant", 15, 138, 25);
+        Monster goblin = new Monster("Goblin", 15, 7, 5);
+        Monster werewolf = new Monster("Werewolf", 11, 58, 7);
 
 //        ArrayList<Monster> monstersList = new ArrayList<>();
         // With arrays, we have to assign values to variables
@@ -70,12 +75,60 @@ public class CollectionsLecture {
 
         // To retrieve an item from a HashMap, use .get() and pass it the key
 
-        System.out.println(careerWins.get("Gregg Popovich"));
+//        System.out.println(careerWins.get("Gregg Popovich"));
 
         // To replace a value, use .replace() and pass it the key
 
         careerWins.replace("Gregg Popovich", 1367);
-        System.out.println(careerWins.get("Gregg Popovich"));
+//        System.out.println(careerWins.get("Gregg Popovich"));
+
+        Map<String, Monster> monsterMap = new HashMap<>();
+
+        // To add multiple entries at once, we can use Map.ofEntries()
+        // This is similar to a long series of entries
+
+        monsterMap = Map.ofEntries(
+                entry("Orc", orc),
+                entry("Blue Dragon", blueDragon),
+                entry("Ogre", ogre),
+                entry("Frost Giant", frostGiant),
+                entry("Goblin", goblin),
+                entry("Werewolf", werewolf)
+        );
+
+        // Map.ofEntries returns an unmodifiable!
+        // This is by design for performance reasons
+        // If I want to be able to modify it, I have to
+        // convert it back to a HashMap, as follows
+
+        Map<String, Monster> monsterHashMap = new HashMap<>(monsterMap);
+
+        // Now the HashMap is modifiable again
+        // How to add a new object to the HashMap:
+        monsterHashMap.put("Skeleton", new Monster("Skeleton", 13, 13, 5));
+
+        // Looping over a HashMap:
+
+        // A set is an unordered collection of unique objects
+
+        // If I want to loop over all the HashMap entries, i can create an EntrySet
+        // An EntrySet is a set of entry object. Entry objects are objects with two properties
+        // a key and a value, and getters for both the key and value
+
+//        for (Map.Entry<String, Monster> monsterEntry : monsterHashMap.entrySet()){
+//            System.out.println("The key is " + monsterEntry.getKey());
+//            System.out.println(monsterEntry.getValue().getName() + " has " + monsterEntry.getValue().getHitPoints() + " Hit Points.");
+//        }
+
+        // There is also a forEach loop
+
+        monsterHashMap.forEach((name, monster)->{ // -> is java's version of the arrow function
+            System.out.println(name + " has " + monster.getHitPoints() + " hit points.");
+        });
+
+        // You can also get a set of the keys using the .keySet method
+        // And you can get a collection of values using the .values() method
+
     }
 
 }
