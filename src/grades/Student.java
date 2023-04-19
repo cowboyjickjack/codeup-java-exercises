168b60b2b926cb115cede6409dc4329fbe269c7c
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-19 10:38:54 -0500

    completed mini arrays/collections exercise

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,6 +1,30 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student() {
+    }
+
+    // returns the student's name
+    public String getName() {
+        return name;
+    }
+    public String setName(String name){
+       return this.name = name;
+    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+    }
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        return 0; // change
+    }
 
 
 }
