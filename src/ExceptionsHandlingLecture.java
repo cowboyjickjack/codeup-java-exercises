7998054cf4cfb8f01bc59d6abf194022344df3c2
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-20 10:48:00 -0500

    began exceptions/error handling lecture

--- src/ExceptionsHandlingLecture.java
+++ src/ExceptionsHandlingLecture.java
@@ -1,16 +1,24 @@
+import java.lang.reflect.Array;
+
 public class ExceptionsHandlingLecture {
     public static void main(String[] args) {
 
         try {
-//            int[] numbers = {1, 2, 3};
+            int[] numbers = {1, 2, 3};
             // since only an array of 3 numbers, it'll say it's 'out of bounds', and throw an error
-//            System.out.println(numbers[99]);
+            System.out.println(numbers[99]);
 
             // you can also throw your own error
             throw new Exception("Something bad happened.");
 
             // in java, you need to name parameter types, and an error in java is an Exception
-        } catch (Exception e){
+            // you can also throw multiple catches
+            // this is referring to the original error we got with the out of bound integer
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
+
+        catch (Exception e){
             // after you throw an exception, you can sout 'e'
             System.out.println(e);
 
